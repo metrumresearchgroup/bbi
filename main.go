9df9802ff2ff2329ec7ebf84a7238e02e9f710bb
Devin Pastoor <devin.pastoor@gmail.com>
2016-03-11 21:13:01 -0500

    clean theta formatting

--- main.go
+++ main.go
@@ -7,12 +7,26 @@ import (
 )
 
 func main() {
-	// file, _ := ioutil.ReadFile("./test/fixtures/blocks/theta-block-01.lst")
-	// // will need to check this also works as expected on windows and doesn't
-	// // keep the \r as well, couldt ry something like runtime.GOOS == "windows"
-	// newFile := strings.Split(string(file), "\n")
-	// for i, f := range newFile {
-	// 	fmt.Println(i, f)
-	// }
-	fmt.Println(nonmemutils.CleanThetaBlock([]string{"$THETA", "0 FIX"}))
+	uglyString := []string{
+		"$THETA (0,2720) ; CL",
+		"$THETA(0,2650) ; V2",
+		" $THETA (0,7730) ; V3",
+		" (0,3410)",
+		" $THETA(0,0.232) FIX ; KA transit",
+		" 0.75 FIX ; allo-WT",
+	}
+	//cleanedString := nonmemutils.CleanThetaBlock([]string{"$THETA", "0 FIX ; bad param", "1 ; CL", "2"})
+	cleanedString := nonmemutils.CleanThetaBlock(uglyString)
+	formattedString := nonmemutils.FormatThetaBlock(cleanedString)
+	for _, line := range uglyString {
+		fmt.Println(line)
+	}
+
+	fmt.Println(" ")
+	fmt.Println("---------- AFTER CLEANING ---------")
+	fmt.Println(" ")
+	fmt.Println("$THETA")
+	for i := range cleanedString {
+		fmt.Println(formattedString[i])
+	}
 }
---
+++ nonmemutils/format_theta.go
@@ -0,0 +1,32 @@
+package nonmemutils
+
+import "strings"
+
+// FormatThetaBlock will format each line in the theta block
+func FormatThetaBlock(lines []string) []string {
+	var result = make([]string, 0)
+	longestCommentIndex := 0
+
+	for _, line := range lines {
+		lineIndex := strings.Index(line, ";")
+		if lineIndex > longestCommentIndex {
+			longestCommentIndex = lineIndex
+		}
+	}
+	// add two extra spaces for more padding
+	// longestLine = longestLine + 2
+	for _, line := range lines {
+		commentIndex := strings.Index(line, ";")
+		var theta string
+		var comment string
+		if commentIndex == -1 {
+			theta = line
+			comment = strings.Repeat(" ", longestCommentIndex-len(line)) + "; <NEED COMMENT>"
+		} else {
+			theta = line[:commentIndex]
+			comment = strings.Repeat(" ", longestCommentIndex-len(theta)) + line[commentIndex:]
+		}
+		result = append(result, theta+comment)
+	}
+	return result
+}
