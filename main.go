496a7e0409705a0e64dc27c1af611ff369968b2e
Devin Pastoor <devin.pastoor@gmail.com>
2016-03-14 18:34:15 -0400

    parse final parameter estimates implemented

--- main.go
+++ main.go
@@ -8,14 +8,65 @@ import (
 
 func main() {
 
-	omegaResult01 := []string{
-		"            ETA1      ETA2",
+	finalParameterEstimates01 := []string{
+		"********************                             FINAL PARAMETER ESTIMATE                           ********************",
+		" ********************                                                                                ********************",
+		" ************************************************************************************************************************",
+		" ",
 		"",
+		" THETA - VECTOR OF FIXED EFFECTS PARAMETERS   *********",
+		"",
+		"         TH 1      TH 2      TH 3      TH 4     ",
+		" ",
+		"         4.79E+00  9.02E+01  7.47E+00  1.05E+02",
+		" ",
+		"",
+		" OMEGA - COV MATRIX FOR RANDOM EFFECTS - ETAS  ********",
+		"",
+		"            ETA1      ETA2   ",
+		" ",
 		" ETA1",
-		"+        1.23E-01",
-		"",
+		"+        1.58E-01",
+		" ",
 		" ETA2",
-		"+        0.00E+00  1.54E-01",
+		"+        1.22E-01  1.33E-01",
+		" ",
+		"",
+		" SIGMA - COV MATRIX FOR RANDOM EFFECTS - EPSILONS  ****",
+		"",
+		"            EPS1      EPS2   ",
+		" ",
+		" EPS1",
+		"+        1.45E+03",
+		" ",
+		" EPS2",
+		"+        0.00E+00  7.39E-03",
+		" ",
+		"1",
+		"",
+		" OMEGA - CORR MATRIX FOR RANDOM EFFECTS - ETAS  *******",
+		"",
+		"",
+		"            ETA1      ETA2   ",
+		" ",
+		" ETA1",
+		"+        3.98E-01",
+		" ",
+		" ETA2",
+		"+        8.42E-01  3.65E-01",
+		" ",
+		"",
+		" SIGMA - CORR MATRIX FOR RANDOM EFFECTS - EPSILONS  ***",
+		"",
+		"            EPS1      EPS2   ",
+		" ",
+		" EPS1",
+		"+        3.81E+01",
+		" ",
+		" EPS2",
+		"+        0.00E+00  8.60E-02",
+		" ",
+		"1",
 	}
 	// uglyString := []string{
 	// 	"$THETA (0,2720) ; CL",
@@ -39,6 +90,6 @@ func main() {
 	// for i := range cleanedString {
 	// 	fmt.Println(formattedString[i])
 	// }
-
-	fmt.Println(parser.ParseOmegaResults(omegaResult01))
+	fmt.Println("parsed Final parameter estimates")
+	fmt.Println(parser.ParseFinalParameterEstimates(finalParameterEstimates01))
 }
---
+++ parser/parse_final_parameter_estimates.go
@@ -0,0 +1,46 @@
+package parser
+
+import (
+	"fmt"
+	"strings"
+)
+
+type FinalParameterEstimates struct {
+	Theta     []string
+	Omega     []string
+	Sigma     []string
+	OmegaCorr []string
+	SigmaCorr []string
+}
+
+func ParseFinalParameterEstimates(lines []string) FinalParameterEstimates {
+	var thetaStart int
+	var omegaStart int
+	var sigmaStart int
+	var omegaCorrStart int
+	var sigmaCorrStart int
+	for i, line := range lines {
+		switch {
+		case strings.Contains(line, "THETA - VECTOR OF FIXED EFFECTS PARAMETERS"):
+			thetaStart = i
+		case strings.Contains(line, "OMEGA - COV MATRIX FOR RANDOM EFFECTS - ETAS"):
+			omegaStart = i
+		case strings.Contains(line, "SIGMA - COV MATRIX FOR RANDOM EFFECTS - EPSILONS"):
+			sigmaStart = i
+		case strings.Contains(line, "OMEGA - CORR MATRIX"):
+			omegaCorrStart = i
+		case strings.Contains(line, "SIGMA - CORR MATRIX"):
+			sigmaCorrStart = i
+		default:
+			continue
+		}
+	}
+	fmt.Println(thetaStart, omegaStart, sigmaStart, omegaCorrStart, sigmaCorrStart)
+	thetaParsed := ParseThetaResults(lines[thetaStart:omegaStart])
+	omegaParsed := ParseBlockResults(lines[omegaStart:sigmaStart])
+	sigmaParsed := ParseBlockResults(lines[sigmaStart:omegaCorrStart])
+	omegaCorrParsed := ParseBlockResults(lines[omegaCorrStart:sigmaCorrStart])
+	sigmaCorrParsed := ParseBlockResults(lines[sigmaCorrStart:])
+
+	return FinalParameterEstimates{thetaParsed, omegaParsed, sigmaParsed, omegaCorrParsed, sigmaCorrParsed}
+}
