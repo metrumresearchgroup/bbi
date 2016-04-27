267f2aeaaf30ad2670422eec37e3618d67247255
Devin Pastoor <devin.pastoor@gmail.com>
2016-04-27 12:24:13 -0400

    start tests on parameter structure

--- parser/parse_parameter_structure_test.go
+++ parser/parse_parameter_structure_test.go
@@ -4,17 +4,28 @@ import (
 	"testing"
 )
 
-var parameterBlock01 = []string{
-	" 1",
-	" 0  2",
-	" 0  2  2",
-	" 0  0  0  3",
-}
-var parameterBlock01Results = []int{1, 0, 2, 0, 2, 2, 0, 0, 0, 3}
+func TestParseParameterStructure(t *testing.T) {
+	var parameterDiagonal01 = []string{"0OMEGA HAS SIMPLE DIAGONAL FORM WITH DIMENSION:   2"}
+	var parameterBlock01 = []string{
+		"0OMEGA HAS BLOCK FORM:",
+		" 1",
+		" 0  2",
+		" 0  2  2",
+		" 0  0  0  3",
+	}
+	var parameterDiagonal01Results = []int{1, 0, 1}
+	var parameterBlock01Results = []int{1, 0, 2, 0, 2, 2, 0, 0, 0, 3}
 
-func TestParseParameterBlock(t *testing.T) {
-	parsedData := ParseBlockStructure(parameterBlock01)
-	for i, val := range parsedData {
+	parsedDiagonal01 := parseParameterStructure(parameterDiagonal01)
+	for i, val := range parsedDiagonal01 {
+		if val != parameterDiagonal01Results[i] {
+			t.Log("GOT: ", val, " EXPECTED: ", parameterBlock01Results[i], " at index: ", i)
+			t.Fail()
+		}
+	}
+
+	parsedBlock01 := parseParameterStructure(parameterBlock01)
+	for i, val := range parsedBlock01 {
 		if val != parameterBlock01Results[i] {
 			t.Log("GOT: ", val, " EXPECTED: ", parameterBlock01Results[i], " at index: ", i)
 			t.Fail()
