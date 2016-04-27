d0575f023799ae5305797dfb0d38229a231b10ab
Devin Pastoor <devin.pastoor@gmail.com>
2016-04-27 12:24:02 -0400

    parameter structure parsing

--- parser/parse_parameter_structure.go
+++ parser/parse_parameter_structure.go
@@ -5,18 +5,48 @@ import (
 	"strings"
 )
 
-// ParseBlockStructure parses the structure of a parameter block
-func ParseBlockStructure(lines []string) []int {
-	var combinedLines string
-	var i int
-	for _, line := range lines {
-		combinedLines += line
-	}
-	strArr := strings.Fields(combinedLines)
-	iArr := make([]int, 0, len(strArr))
-	for _, str := range strArr {
-		i, _ = strconv.Atoi(str)
-		iArr = append(iArr, i)
+// ParameterStructures contains the final parameter estimate values
+type ParameterStructures struct {
+	Theta int
+	Omega []int
+	Sigma []int
+}
+
+func parseParameterStructure(lines []string) []int {
+	var iArr []int
+	if strings.Contains(lines[0], "SIMPLE") {
+		omegaLength, _ := strconv.Atoi(strings.TrimSpace(strings.SplitAfter(lines[0], ":")[1]))
+		iArr = createDiagonalBlock(omegaLength)
+	} else {
+		iArr = ParseBlockStructure(lines[1:])
 	}
 	return iArr
+}
+
+// ParseParameterStructures parses the final estimates of model parameters from lst file
+func ParseParameterStructures(lines []string) ParameterStructures {
+	var thetaLength int
+	var omegaFormStart int
+	var omegaFormEnd int
+	var sigmaFormStart int
+	var sigmaFormEnd int
+	for i, line := range lines {
+		switch {
+		case strings.Contains(line, "0LENGTH OF THETA:"):
+			thetaLength, _ = strconv.Atoi(strings.TrimSpace(strings.SplitAfter(line, ":")[1]))
+		case strings.Contains(line, "0OMEGA HAS "):
+			omegaFormStart = i
+		case strings.Contains(line, "0DEFAULT OMEGA BOUNDARY"):
+			omegaFormEnd = i
+		case strings.Contains(line, "0SIGMA HAS "):
+			sigmaFormStart = i
+		case strings.Contains(line, "0DEFAULT SIGMA BOUNDARY"):
+			sigmaFormEnd = i
+		default:
+			continue
+		}
+	}
+	omegaStructure := parseParameterStructure(lines[omegaFormStart:omegaFormEnd])
+	sigmaStructure := parseParameterStructure(lines[sigmaFormStart:sigmaFormEnd])
+	return ParameterStructures{thetaLength, omegaStructure, sigmaStructure}
 }
