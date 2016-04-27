5c0697b77f4c575edf52b5b463cf18994b421ec6
Devin Pastoor <devin.pastoor@gmail.com>
2016-04-27 10:21:12 -0400

    initial parsing of block structure for parameters

---
+++ parser/parse_parameter_structure.go
@@ -0,0 +1,22 @@
+package parser
+
+import (
+	"strconv"
+	"strings"
+)
+
+// ParseBlockStructure parses the structure of a parameter block
+func ParseBlockStructure(lines []string) []int {
+	var combinedLines string
+	var i int
+	for _, line := range lines {
+		combinedLines += line
+	}
+	strArr := strings.Fields(combinedLines)
+	iArr := make([]int, 0, len(strArr))
+	for _, str := range strArr {
+		i, _ = strconv.Atoi(str)
+		iArr = append(iArr, i)
+	}
+	return iArr
+}
