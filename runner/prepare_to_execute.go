cba62c052d808b74b4451c8d270ceaa4db94c9e0
Devin Pastoor <devin.pastoor@gmail.com>
2016-11-26 13:08:37 -0500

    handle path replacement in runner

--- runner/prepare_to_execute.go
+++ runner/prepare_to_execute.go
@@ -1,7 +1,15 @@
 package runner
 
+import "strings"
+import "github.com/dpastoor/nonmemutils/parser"
+
 //PrepareForExecution parses and prepares strings from a file for execution in a different context
 // for example, replacing the $DATA path
 func PrepareForExecution(s []string) []string {
+	for i, line := range s {
+		if strings.Contains(line, "$DATA") {
+			s[i] = parser.AddPathLevelToData(line)
+		}
+	}
 	return s
 }
