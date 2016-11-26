14e4209c13fd33002c628daf18b5f925911dda35
Devin Pastoor <devin.pastoor@gmail.com>
2016-11-26 15:58:42 -0500

    add writelines and new FS method that works with afero fs

--- utils/readLines.go
+++ utils/readLines.go
@@ -2,7 +2,10 @@ package utils
 
 import (
 	"bufio"
+	"fmt"
 	"os"
+
+	"github.com/spf13/afero"
 )
 
 //ReadLines reads lines for a file at a given path
@@ -19,4 +22,35 @@ func ReadLines(path string) ([]string, error) {
 		lines = append(lines, scanner.Text())
 	}
 	return lines, nil
+}
+
+//ReadLinesFS reads lines for a file at a given path
+func ReadLinesFS(fs afero.Fs, path string) ([]string, error) {
+	inFile, err := fs.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer inFile.Close()
+	scanner := bufio.NewScanner(inFile)
+	scanner.Split(bufio.ScanLines)
+	var lines []string
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines, nil
+}
+
+//WriteLines writes lines to a file at a given path
+func WriteLines(lines []string, path string) error {
+	file, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	w := bufio.NewWriter(file)
+	for _, line := range lines {
+		fmt.Fprintln(w, line)
+	}
+	return w.Flush()
 }
