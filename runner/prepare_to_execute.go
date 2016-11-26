d64237254e0f1a26ca61c7ec374908ba9b3b2cb0
Devin Pastoor <devin.pastoor@gmail.com>
2016-11-26 10:38:37 -0500

    start runner package and implement first red test for preparing lines for execution

---
+++ runner/prepare_to_execute.go
@@ -0,0 +1,7 @@
+package runner
+
+//PrepareForExecution parses and prepares strings from a file for execution in a different context
+// for example, replacing the $DATA path
+func PrepareForExecution(s []string) []string {
+	return s
+}
