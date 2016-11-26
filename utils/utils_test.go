655d3af06e72fa603cd3da856b88f8a013b6b69a
Devin Pastoor <devin.pastoor@gmail.com>
2016-11-26 15:18:12 -0500

    test for file and ext

--- utils/utils_test.go
+++ utils/utils_test.go
@@ -58,3 +58,30 @@ func TestIsDir(t *testing.T) {
 		}
 	}
 }
+
+func TestFileAndExt(t *testing.T) {
+	type fileAndExt struct {
+		file string
+		ext  string
+	}
+	type test struct {
+		input    string
+		expected fileAndExt
+	}
+	data := []test{
+		{"test.txt", fileAndExt{"test", ".txt"}},
+		{"path/test.txt", fileAndExt{"test", ".txt"}},
+		{"../relativepath/test.txt", fileAndExt{"test", ".txt"}},
+		{"/absolutepath/test.txt", fileAndExt{"test", ".txt"}},
+	}
+
+	for i, d := range data {
+		file, ext := FileAndExt(d.input)
+		if file != d.expected.file {
+			t.Errorf("Test %d failed. Expected %s got %s", i, d.expected.file, file)
+		}
+		if ext != d.expected.ext {
+			t.Errorf("Test %d failed. Expected %s got %s", i, d.expected.ext, ext)
+		}
+	}
+}
