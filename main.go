f5c3049165359986d98fed4722df3574bdc236bc
Devin Pastoor <devin.pastoor@gmail.com>
2016-11-27 17:25:10 -0500

    first working implementation initializing files to execute

--- main.go
+++ main.go
@@ -4,20 +4,35 @@ import (
 	"fmt"
 	"path/filepath"
 
+	"github.com/dpastoor/nonmemutils/runner"
 	"github.com/dpastoor/nonmemutils/utils"
 	"github.com/spf13/afero"
 )
 
 func main() {
 	AppFs := afero.NewOsFs()
-	filePath := "parser/fixtures/lstfiles/simple-onecmpt-ex1.lst"
-	fileName, _ := utils.FileAndExt(filePath)
+	filePath := "fixtures/run001.mod"
+
+	// create a new dir for model estimation
+	fileName, fileExt := utils.FileAndExt(filePath)
 	dir := filepath.Dir(filePath)
-	upOneLevel := filepath.Join(dir, "..")
-	dirInfo, _ := afero.ReadDir(AppFs, upOneLevel)
-	for i, line := range dirInfo {
-		if line.IsDir() {
-			fmt.Printf("%v: %s\n", i, line.Name())
-		}
-	}
+	dirInfo, _ := afero.ReadDir(AppFs, dir)
+	dirs := utils.ListDirNames(dirInfo)
+	newDirSuggestion := runner.FindNextEstDirNum(fileName, dirs, 2)
+	AppFs.MkdirAll(filepath.Join(
+		dir,
+		newDirSuggestion.NextDirName,
+	), 0755)
+
+	// prepare and copy the model to be run001
+	fileLines, _ := utils.ReadLinesFS(AppFs, filePath)
+	utils.WriteLinesFS(
+		AppFs,
+		runner.PrepareForExecution(fileLines),
+		filepath.Join(
+			dir,
+			newDirSuggestion.NextDirName,
+			fmt.Sprintf("%s%s", fileName, fileExt),
+		),
+	)
 }
