f7d087aaecebecc18cfb4d79523eb094dff7225a
Devin Pastoor <devin.pastoor@gmail.com>
2016-11-28 16:11:21 -0500

    add redirects for messages to stdout

--- stdout_stream.R
+++ stdout_stream.R
@@ -1,10 +1,13 @@
 print_to_stdout_short <- function() {
     message("first message, about to send messages at 0.5 second intervals")
+    print("a printed message")
+    warning("a warning")
     for (i in 1:4) {
         Sys.sleep(0.5)
         message("message: ", i, " - going back to sleep now...")
     }
     return(TRUE)
 }
+sink(stdout(), type = "message") # sink messages to stdout
 
 print_to_stdout_short()
