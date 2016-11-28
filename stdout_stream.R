698b80e06beef799fd2d533a0dd37cef6fae8587
Devin Pastoor <devin.pastoor@gmail.com>
2016-11-28 16:11:44 -0500

    add redirects for messages to stdout

--- stdout_stream.R
+++ stdout_stream.R
@@ -4,7 +4,7 @@ print_to_stdout_short <- function() {
     warning("a warning")
     for (i in 1:4) {
         Sys.sleep(0.5)
-        message("message: ", i, " - going back to sleep now...")
+        messae("message: ", i, " - going back to sleep now...")
     }
     return(TRUE)
 }
