e808bcebfffc29c8f00afb04acf9ea0bd42f3b7a
Devin Pastoor <devin.pastoor@gmail.com>
2016-12-07 19:36:42 -0500

    test pattern to extract any defined files from model file

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "github.com/dpastoor/nonmemutils/cmd"
+import (
+	"fmt"
+	"regexp"
+	"strings"
+)
 
 func main() {
-	cmd.Execute()
+	testStrings := []string{
+		"hello here is a FILE",
+		"$TABLE CL V FILE=patab001",
+		"$TABLE O O2 FILE = sdtab001",
+		"$TABLE O O2 FILE = edtab001.ext",
+		"$TABLE O O2 FILE = mdtab001.ext MORE",
+	}
+	// pattern FILE, maybe whitespace, =, maybe whitespace, word (non-whitespace)
+	r, _ := regexp.Compile("FILE\\s?=\\s?(\\S+)")
+	for _, line := range testStrings {
+		fmt.Println(strings.Contains(line, "FILE="))
+		match := r.FindStringSubmatch(line)
+		if len(match) > 0 {
+			fmt.Println(match[1])
+		}
+	}
 }
