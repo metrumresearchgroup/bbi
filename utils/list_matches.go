686b6498a13015d236dc7fcdd79dc9073ed8288c
Devin Pastoor <devin.pastoor@gmail.com>
2016-12-13 14:20:06 -0500

    match by regex implemented as precursor to cleaning functions

---
+++ utils/list_matches.go
@@ -0,0 +1,25 @@
+package utils
+
+import (
+	"errors"
+
+	"regexp"
+)
+
+// ListMatchesByRegex returns all matches by a regex
+// as regexp.MustCompile panics if the regex does not compile, the function
+// is designed to recover and return an error immediately
+func ListMatchesByRegex(names []string, regex string) (matches []string, err error) {
+	defer func() {
+		if r := recover(); r != nil {
+			err = errors.New("could not parse regex")
+		}
+	}()
+	r := regexp.MustCompile(regex)
+	for _, n := range names {
+		if r.MatchString(n) {
+			matches = append(matches, n)
+		}
+	}
+	return
+}
---
+++ utils/list_matches_test.go
@@ -0,0 +1,61 @@
+package utils
+
+import "testing"
+
+func TestListMatchesByRegex(t *testing.T) {
+	type input struct {
+		Names []string
+		Regex string
+	}
+	type test struct {
+		input    input
+		expected []string
+	}
+	exData := []string{
+		"run001.mod",
+		"run002.lst",
+		"run003.mod",
+		"nota.run",
+	}
+
+	data := []test{
+		{
+			input{
+				Names: exData,
+				Regex: "run00..mod",
+			},
+			[]string{
+				"run001.mod",
+				"run003.mod",
+			},
+		},
+		{
+			input{
+				Names: exData,
+				Regex: "run$",
+			},
+			[]string{
+				"nota.run",
+			},
+		},
+		{
+			input{
+				Names: exData,
+				Regex: "^run",
+			},
+			[]string{
+				"run001.mod",
+				"run003.mod",
+			},
+		},
+	}
+
+	for i, d := range data {
+		res, _ := ListMatchesByRegex(d.input.Names, d.input.Regex)
+		for j, expected := range res {
+			if d.expected[j] != expected {
+				t.Errorf("Test %d failed. Expected %s got %s", i, d.expected[j], expected)
+			}
+		}
+	}
+}
