4a5cb1fd576ff731d45197743f95580f1370fdcb
Devin Pastoor <devin.pastoor@gmail.com>
2017-01-09 15:34:12 -0500

    os specific test due to path differences in sep

--- runner/prepare_to_execute_test.go
+++ runner/prepare_to_execute_test.go
@@ -2,40 +2,76 @@ package runner
 
 import (
 	"fmt"
+	"runtime"
 	"testing"
 )
 
-var pathtests = []struct {
-	in  []string
-	out []string
-}{
-	{
-		[]string{
-			"$PROB",
-			"$DATA ../modeling.csv",
-			"$PK",
-		},
-		[]string{
-			"$PROB",
-			"$DATA ../../modeling.csv",
-			"$PK",
-		},
-	},
-	{
-		[]string{
-			"$PROB",
-			"$DATA ../modeling.csv IGNORE=@",
-			"$PK",
-		},
-		[]string{
-			"$PROB",
-			"$DATA ../../modeling.csv IGNORE=@",
-			"$PK",
-		},
-	},
-}
-
 func TestPrepareForExecution(t *testing.T) {
+	var pathtests []struct {
+		in  []string
+		out []string
+	}
+	if runtime.GOOS == "windows" {
+		pathtests = []struct {
+			in  []string
+			out []string
+		}{
+			{
+				[]string{
+					"$PROB",
+					"$DATA ../modeling.csv",
+					"$PK",
+				},
+				[]string{
+					"$PROB",
+					"$DATA ..\\..\\modeling.csv",
+					"$PK",
+				},
+			},
+			{
+				[]string{
+					"$PROB",
+					"$DATA ..\\modeling.csv IGNORE=@",
+					"$PK",
+				},
+				[]string{
+					"$PROB",
+					"$DATA ..\\..\\modeling.csv IGNORE=@",
+					"$PK",
+				},
+			},
+		}
+	} else {
+		pathtests = []struct {
+			in  []string
+			out []string
+		}{
+			{
+				[]string{
+					"$PROB",
+					"$DATA ../modeling.csv",
+					"$PK",
+				},
+				[]string{
+					"$PROB",
+					"$DATA ../../modeling.csv",
+					"$PK",
+				},
+			},
+			{
+				[]string{
+					"$PROB",
+					"$DATA ../modeling.csv IGNORE=@",
+					"$PK",
+				},
+				[]string{
+					"$PROB",
+					"$DATA ../../modeling.csv IGNORE=@",
+					"$PK",
+				},
+			},
+		}
+	}
 	for _, tt := range pathtests {
 		res := PrepareForExecution(tt.in)
 		for i, l := range res {
