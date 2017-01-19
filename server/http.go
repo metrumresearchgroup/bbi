6c49883068e947f5bea8a2e93d132725a9705209
Devin Pastoor <devin.pastoor@gmail.com>
2017-01-19 18:25:10 -0500

    unnecessary handler didn't really do anything

--- server/http.go
+++ server/http.go
@@ -2,24 +2,12 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"net/http"
 	"strconv"
 
 	"github.com/pressly/chi"
 	"github.com/pressly/chi/render"
 )
-
-func (ms *ModelStore) handleGetModelID(w http.ResponseWriter, r *http.Request) {
-	ctx := r.Context()
-	model, ok := ctx.Value("model").(*Model)
-
-	if !ok {
-		http.Error(w, http.StatusText(422), 422)
-		return
-	}
-	w.Write([]byte(fmt.Sprintf("modelID:%v", model.ID)))
-}
 
 func (ms *ModelStore) handleGetModel(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
