3de5b17c1add0aee39e2db4784fb937ed0601a8c
Devin Pastoor <devin.pastoor@gmail.com>
2017-01-28 14:42:28 -0500

    update runner to use new optional arg settings

--- cmd/run.go
+++ cmd/run.go
@@ -151,15 +151,15 @@ func runModel(
 		fs,
 		filePath,
 		runner.RunSettings{
-			Git:     viper.GetBool("git"),
-			SaveExe: saveExe,
+			Git:            viper.GetBool("git"),
+			SaveExe:        saveExe,
+			Verbose:        verbose,
+			Debug:          debug,
+			CleanLvl:       cleanLvl,
+			CopyLvl:        copyLvl,
+			CacheDir:       viper.GetString("cacheDir"),
+			ExeNameInCache: viper.GetString("cacheExe"),
 		},
-		verbose,
-		debug,
-		cleanLvl,
-		copyLvl,
-		viper.GetString("cacheDir"),
-		viper.GetString("cacheExe"),
 	)
 	if verbose {
 		log.Printf("completed run %s releasing worker back to queue \n", filePath)
