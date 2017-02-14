c4454ba67754e134db3f8e10e671e199c62433bc
Devin Pastoor <devin.pastoor@gmail.com>
2017-02-13 22:46:19 -0500

    inverse regex listing implementation

--- utils/list_matches.go
+++ utils/list_matches.go
@@ -23,3 +23,21 @@ func ListMatchesByRegex(names []string, regex string) (matches []string, err err
 	}
 	return
 }
+
+// ListNonMatchesByRegex returns all matches by a regex
+// as regexp.MustCompile panics if the regex does not compile, the function
+// is designed to recover and return an error immediately
+func ListNonMatchesByRegex(names []string, regex string) (nonmatches []string, err error) {
+	defer func() {
+		if r := recover(); r != nil {
+			err = errors.New("could not parse regex")
+		}
+	}()
+	r := regexp.MustCompile(regex)
+	for _, n := range names {
+		if !r.MatchString(n) {
+			nonmatches = append(nonmatches, n)
+		}
+	}
+	return
+}
