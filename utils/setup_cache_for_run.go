3261d1212746b4514e4ddb02152e71e5cf60e7a2
Devin Pastoor <devin.pastoor@gmail.com>
2017-06-19 21:25:47 -0400

    handle if given absolute path to start

--- utils/setup_cache_for_run.go
+++ utils/setup_cache_for_run.go
@@ -18,12 +18,22 @@ func SetupCacheForRun(
 	cacheDir string,
 	nmNameInCache string,
 ) error {
+	var fullCacheDirPath string
+	var fullModelDirPath string
+	if filepath.IsAbs(cacheDir) {
+		fullCacheDirPath = cacheDir
+	} else {
+		fullCacheDirPath = filepath.Join(baseDir, cacheDir)
+	}
 
-	fullCacheDirPath := filepath.Join(baseDir, cacheDir)
+	if filepath.IsAbs(modelDir) {
+		fullModelDirPath = modelDir
+	} else {
+		fullModelDirPath = filepath.Join(baseDir, modelDir)
+	}
 	// this should always be there how the package is being used currently as the time
 	// this function is called is after the modelDir is successfully created, but given
 	// there may be other uses, to be safe will also check again.
-	fullModelDirPath := filepath.Join(baseDir, modelDir)
 	ok, err := DirExists(fullCacheDirPath, fs)
 	if !ok || err != nil {
 		//TODO: change these exits to instead just return an error probably
