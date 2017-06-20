4ee36f4f1f18c170a4b4e62c96eb8d7d8a177dd5
Devin Pastoor <devin.pastoor@gmail.com>
2017-06-20 19:49:37 -0400

    push back out whether is the first run, and fix incorrect reOrg test

--- runner/find_next_dir_num_test.go
+++ runner/find_next_dir_num_test.go
@@ -14,6 +14,7 @@ var nextModelDirTests = []struct {
 		NextDirSuggestion{
 			"run001_est_01",
 			false,
+			true,
 		},
 	},
 	{
@@ -23,6 +24,7 @@ var nextModelDirTests = []struct {
 		},
 		NextDirSuggestion{
 			"run001_est_03",
+			false,
 			false,
 		},
 	},
@@ -34,15 +36,24 @@ var nextModelDirTests = []struct {
 		NextDirSuggestion{
 			"run001_est_06",
 			true,
+			false,
 		},
 	},
 }
 
 func TestFindNextEstDir(t *testing.T) {
-	for _, tt := range nextModelDirTests {
+	for i, tt := range nextModelDirTests {
 		data := FindNextEstDirNum("run001", tt.in, 2)
 		if data.NextDirName != tt.out.NextDirName {
-			t.Log(fmt.Sprintf("GOT: %s, EXPECTED: %s", data.NextDirName, tt.out.NextDirName))
+			t.Log(fmt.Sprintf("test %v, Incorrect NextDirName: GOT: %s, EXPECTED: %s", i, data.NextDirName, tt.out.NextDirName))
+			t.Fail()
+		}
+		if data.FirstRun != tt.out.FirstRun {
+			t.Log(fmt.Sprintf("test %v, Incorrect FirstRun: GOT: %v, EXPECTED: %v", i, data.FirstRun, tt.out.FirstRun))
+			t.Fail()
+		}
+		if data.Reorg != tt.out.Reorg {
+			t.Log(fmt.Sprintf("test %v Incorrect Reorg: GOT: %v, EXPECTED: %v", i, data.Reorg, tt.out.Reorg))
 			t.Fail()
 		}
 	}
