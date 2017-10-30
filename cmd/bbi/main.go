322f4f9c7c4d7b0d26e38984861fdd0b14698bd1
Devin Pastoor <devin.pastoor@gmail.com>
2017-10-30 17:22:26 -0400

    note about generating docs

--- cmd/bbi/main.go
+++ cmd/bbi/main.go
@@ -1,7 +1,15 @@
 package main
 
-import "github.com/dpastoor/babylon/cmd"
+import (
+	"github.com/dpastoor/babylon/cmd"
+)
 
+// if want to generate docs
+//	import "github.com/spf13/cobra/doc"
+//	err := doc.GenMarkdownTree(cmd.RootCmd, "../../docs/bbi")
+//	if err != nil {
+//		panic(err)
+//	}
 func main() {
 	cmd.Execute()
 }
