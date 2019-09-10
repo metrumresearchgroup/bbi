e15250cca761a521846a6f22716c59be3e6eded6
David <davidl@metrumrg.com>
2019-09-10 16:55:37 -0400

    test: remove extra marshalling; allow bbi exe to be found by path

--- testdata/summary_test.go
+++ testdata/summary_test.go
@@ -34,7 +34,7 @@ func TestSummary(t *testing.T) {
 			goldenFile: "./example-models/nonmem/NonLinearCL/NonLinearCL.json",
 		},
 	}
-	bbiExe := "/usr/local/bin/bbi"
+	bbiExe := "bbi"
 	osFs := afero.NewOsFs()
 	for _, tt := range tests {
 		context := filepath.Base(tt.goldenFile)
@@ -48,22 +48,6 @@ func TestSummary(t *testing.T) {
 		err = json.Unmarshal(goldenJSON, &goldenSummary)
 		assert.Equal(t, nil, err, fmt.Sprintf("[%s] fail to unmarshal goldenSummary: %s", context, err))
 
-		// marshal golden objects to bytes
-		goldenParametersData, err := json.Marshal(goldenSummary.ParametersData)
-		assert.Equal(t, nil, err, fmt.Sprintf("[%s] fail to marshal golden ParametersData: %s", context, err))
-
-		goldenParametersStructures, err := json.Marshal(goldenSummary.ParameterStructures)
-		assert.Equal(t, nil, err, fmt.Sprintf("[%s] fail to marshal golden ParameterStructures: %s", context, err))
-
-		goldenParametersNames, err := json.Marshal(goldenSummary.ParameterNames)
-		assert.Equal(t, nil, err, fmt.Sprintf("[%s] fail to marshal golden ParameterNames: %s", context, err))
-
-		goldenOfv, err := json.Marshal(goldenSummary.OFV)
-		assert.Equal(t, nil, err, fmt.Sprintf("[%s] fail to marshal golden OFV: %s", context, err))
-
-		goldenShrinkageDetails, err := json.Marshal(goldenSummary.ShrinkageDetails)
-		assert.Equal(t, nil, err, fmt.Sprintf("[%s] fail to marshal golden ShrinkageDetails: %s", context, err))
-
 		// execute bbi and capture summary output as string
 		stdout, err := exec.Command(bbiExe, "summary", "--tree", tt.modFile).Output()
 		assert.Equal(t, nil, err, fmt.Sprintf("[%s] fail exec %s: %s", context, bbiExe, err))
@@ -72,27 +56,11 @@ func TestSummary(t *testing.T) {
 		var bbiSummary parser.ModelOutput
 		err = json.Unmarshal(stdout, &bbiSummary)
 		assert.Equal(t, nil, err, fmt.Sprintf("[%s] fail to unmarshal summary: %s", context, err))
-
-		// marshal summary data to bytes and compare to golden data
-		summaryParametersData, err := json.Marshal(bbiSummary.ParametersData)
 		assert.Equal(t, nil, err, fmt.Sprintf("[%s] fail to marshal summary ParametersData: %s", context, err))
-		assert.Equal(t, goldenParametersData, summaryParametersData, fmt.Sprintf("[%s] parametersData not equal", context))
-
-		summaryParametersStructures, err := json.Marshal(bbiSummary.ParameterStructures)
-		assert.Equal(t, nil, err, fmt.Sprintf("[%s] fail to marshal summary ParameterStructures: %s", context, err))
-		assert.Equal(t, goldenParametersStructures, summaryParametersStructures, fmt.Sprintf("[%s] parametersStructures not equal", context))
-
-		summaryParametersNames, err := json.Marshal(bbiSummary.ParameterNames)
-		assert.Equal(t, nil, err, fmt.Sprintf("[%s] fail to marshal summary ParameterNames: %s", context, err))
-		assert.Equal(t, goldenParametersNames, summaryParametersNames, fmt.Sprintf("[%s] parametersStructures not equal", context))
-
-		summaryOfv, err := json.Marshal(bbiSummary.OFV)
-		assert.Equal(t, nil, err, fmt.Sprintf("[%s] fail to marshal summary OFV: %s", context, err))
-		assert.Equal(t, goldenOfv, summaryOfv, fmt.Sprintf("[%s] parametersStructures not equal", context))
-
-		summaryShrinkageDetails, err := json.Marshal(bbiSummary.ShrinkageDetails)
-		assert.Equal(t, nil, err, fmt.Sprintf("[%s] fail to marshal summary ShrinkageDetails: %s", context, err))
-		assert.Equal(t, goldenShrinkageDetails, summaryShrinkageDetails, fmt.Sprintf("[%s] parametersStructures not equal", context))
-
+		assert.Equal(t, goldenSummary.ParametersData, bbiSummary.ParametersData, fmt.Sprintf("[%s] parametersData not equal", context))
+		assert.Equal(t, goldenSummary.ParameterStructures, bbiSummary.ParameterStructures, fmt.Sprintf("[%s] parametersStructures not equal", context))
+		assert.Equal(t, goldenSummary.ParameterNames, bbiSummary.ParameterNames, fmt.Sprintf("[%s] parametersStructures not equal", context))
+		assert.Equal(t, goldenSummary.OFV, bbiSummary.OFV, fmt.Sprintf("[%s] parametersStructures not equal", context))
+		assert.Equal(t, goldenSummary.ShrinkageDetails, bbiSummary.ShrinkageDetails, fmt.Sprintf("[%s] parametersStructures not equal", context))
 	}
 }
