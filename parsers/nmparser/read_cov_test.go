03cf06f04e27435128de82b948b613fe8c6a920e
Devin Pastoor <devin.pastoor@gmail.com>
2019-11-22 10:36:49 -0500

    test: refactor table test

--- parsers/nmparser/read_cov_test.go
+++ parsers/nmparser/read_cov_test.go
@@ -8,9 +8,29 @@ import (
 
 func TestReadParseCovLines(t *testing.T) {
 	var tests = []struct {
-		lines   []string
-		context string
+		lines    []string
+		context  string
+		nMethods int
+		nThetas  int
 	}{
+		{
+			lines: []string{
+				"TABLE NO.     1: First Order Conditional Estimation with Interaction: Problem=1 Subproblem=0 Superproblem1=0 Iteration1=0 Superproblem2=0 Iteration2=0",
+				" NAME         THETA1       THETA2       THETA3       THETA4       THETA5       THETA6       THETA7       THETA8       THETA9      ",
+				" THETA1        3.70979E-01 -2.14341E-01 -1.50899E-01 -2.41113E-02  1.04312E-03  0.00000E+00  0.00000E+00  0.00000E+00  0.00000E+00",
+				" THETA2       -2.14341E-01  1.94116E+01 -3.53392E+00 -1.99270E+00  3.64529E-02  0.00000E+00  0.00000E+00  0.00000E+00  0.00000E+00",
+				" THETA3       -1.50899E-01 -3.53392E+00  5.42576E+00  1.44045E+00 -2.83908E-02  0.00000E+00  0.00000E+00  0.00000E+00  0.00000E+00",
+				" THETA4       -2.41113E-02 -1.99270E+00  1.44045E+00  1.02609E+00 -1.57357E-02  0.00000E+00  0.00000E+00  0.00000E+00  0.00000E+00",
+				" THETA5        1.04312E-03  3.64529E-02 -2.83908E-02 -1.57357E-02  4.52737E-04  0.00000E+00  0.00000E+00  0.00000E+00  0.00000E+00",
+				" THETA6        6.00000E+00  0.00000E+00  0.00000E+00  0.00000E+00  0.00000E+00  0.00000E+00  0.00000E+00  0.00000E+00  0.00000E+00",
+				" THETA7        7.00000E+00  0.00000E+00  0.00000E+00  0.00000E+00  0.00000E+00  0.00000E+00  0.00000E+00  0.00000E+00  0.00000E+00",
+				" THETA8        8.00000E+00  0.00000E+00  0.00000E+00  0.00000E+00  0.00000E+00  0.00000E+00  0.00000E+00  0.00000E+00  0.00000E+00",
+				" THETA9        9.00000E+00  0.00000E+00  0.00000E+00  0.00000E+00  0.00000E+00  0.00000E+00  0.00000E+00  0.00000E+00  9.90000E+00",
+			},
+			context:  "only thetas",
+			nMethods: 1,
+			nThetas:  9,
+		},
 		{
 			lines: []string{
 				"TABLE NO.     1: First Order Conditional Estimation with Interaction: Problem=1 Subproblem=0 Superproblem1=0 Iteration1=0 Superproblem2=0 Iteration2=0",
@@ -70,16 +90,17 @@ func TestReadParseCovLines(t *testing.T) {
 				" OMEGA(3,2)    0.00000E+00  0.00000E+00  0.00000E+00  0.00000E+00  0.00000E+00  0.00000E+00  0.00000E+00  0.00000E+00  0.00000E+00  0.00000E+00  0.00000E+00  0.00000E+00  0.00000E+00  0.00000E+00  0.00000E+00  0.00000E+00",
 				" OMEGA(3,3)    1.02250E-04 -2.16771E-04  1.78136E-04 -5.31316E-05  2.49581E-06  0.00000E+00  0.00000E+00  0.00000E+00  0.00000E+00 -1.05448E-08  1.58649E-06  0.00000E+00 -3.22735E-07  0.00000E+00  0.00000E+00  3.47626E-06",
 			},
-			context: "no zero",
+			context:  "full control stream with multiple methods",
+			nMethods: 3,
+			nThetas:  9,
 		},
 	}
 
 	for _, tt := range tests {
 		extData := parseCovLines(tt.lines)
+		assert.Equal(t, len(extData.EstimationMethods), tt.nMethods, "failed to extract separate tables")
 		assert.Equal(t, tt.lines[0], extData.EstimationMethods[0], "Fail :"+tt.context)
-		assert.Equal(t, "TABLE NO.     2: First Order Conditional Estimation with Interaction: Problem=1 Subproblem=0 Superproblem1=0 Iteration1=0 Superproblem2=0 Iteration2=0", extData.EstimationMethods[1], "Fail :"+tt.context)
-		assert.Equal(t, "TABLE NO.     3: First Order Conditional Estimation with Interaction: Problem=1 Subproblem=0 Superproblem1=0 Iteration1=0 Superproblem2=0 Iteration2=0", extData.EstimationMethods[2], "Fail :"+tt.context)
-
+		t.Log("heres a log message in EstimationMethods")
 		res := GetThetaValues(tt.lines)
 		for _, fa := range res {
 			assert.Equal(t, -0.214341, fa.Values[9], "Fail :"+tt.context)
