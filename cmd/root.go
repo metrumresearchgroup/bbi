3bd051038ac8181b70bfab8773d9443a372bc453
Devin Pastoor <devinp@metrumrg.com>
2021-01-03 09:50:05 -0500

    testing: add two table test
    [km] Updated variable name to resolve conflicts with 605790c (cmd:
         convert cobra style commands to testable components, 2021-09-08).

--- cmd/root.go
+++ cmd/root.go
@@ -47,6 +47,7 @@ var (
 	Json               bool
 	preview            bool
 	executionWaitGroup sync.WaitGroup
+	output             string
 )
 
 // Execute adds all child commands to the root command sets flags appropriately.
@@ -91,6 +92,9 @@ func NewRootCmd() *cobra.Command {
 	cmd.PersistentFlags().BoolVar(&Json, "json", false, "json tree of output, if possible")
 	errpanic(viper.BindPFlag("json", cmd.PersistentFlags().Lookup("json"))) // Bind to viper
 	cmd.PersistentFlags().BoolVarP(&preview, "preview", "p", false, "preview action, but don't actually run command")
+
+	viper.BindPFlag("output", cmd.PersistentFlags().Lookup("output")) //Bind to viper
+	cmd.PersistentFlags().StringVarP(&output, "output", "o", "", "output file")
 
 	cmd.AddCommand(NewInitCmd())
 	cmd.AddCommand(NewNonmemCmd())
