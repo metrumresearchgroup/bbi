e8f4788a64c6a578fa0633f57cb740df6770f830
Kyle Meyer <kylem@metrumrg.com>
2024-08-13 08:20:33 -0400

    cmd/bbi/main.go: delete commented code for generating docs
    This will be handled by a dedicated executable.

--- cmd/bbi/main.go
+++ cmd/bbi/main.go
@@ -7,13 +7,6 @@ import (
 // buildTime  can be set from LDFLAGS during development.
 var buildTime string
 
-// if want to generate docs
-//
-//	import "github.com/spf13/cobra/doc"
-//	err := doc.GenMarkdownTree(cmd.RootCmd, "../../docs/bbi")
-//	if err != nil {
-//		panic(err)
-//	}
 func main() {
 	cmd.Execute(buildTime)
 }
